93addf18281de523d7065006dc2262777ba0f5d9
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-08-11 22:59:05 -0400

    auth setup

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Landing = () => {
-  return <div className="text-blue-500">Landing</div>;
+  return (
+    <div className="text-blue-500">
+      <h1>Landinf</h1>
+    </div>
+  );
 };
 
 export default Landing;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Landing from "./landing/page";
 import { SignOut } from "@/components/Logout";
-import CheckS from "@/components/CheckS";
+
 import LoginLogoutButton from "@/components/LoginOrOut";
-import End from "@/components/End";
 
 const page = () => {
   return (
     <>
       <Landing />
-      <LoginLogoutButton />
-      <End />
     </>
   );
 };
