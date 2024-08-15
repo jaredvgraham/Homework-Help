3eb56c9a3e156de572546de5e5c80c5972c4bb56
evanwilson2123 <evanmwilson@gmail.com>
2024-08-14 20:47:02 -0400

    whatever

---
+++ src/models/PostModel.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IPost extends Document {
+  message: string;
+  assignment: mongoose.Types.ObjectId[];
+  postType: "assignment" | "announcement";
+  class: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostSchema: Schema = new Schema({
+  message: { type: String, required: true },
+  assignment: [
+    { type: Schema.Types.ObjectId, ref: "Assignment", required: false },
+  ],
+  postType: {
+    postType: String,
+    enum: ["assignment", "announcement"],
+    default: "announcement",
+  },
+  class: [{ type: Schema.Types.ObjectId, ref: "Class", required: false }],
+});
+
+const Post = mongoose.models.Post || mongoose.model<IPost>("Post", PostSchema);
+
+export default Post;
