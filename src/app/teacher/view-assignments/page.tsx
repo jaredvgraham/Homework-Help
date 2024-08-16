5df792b8af6eb80904d691bf6c2a4fee6f5a5bcf
JaredvGraham <jgram153@icloud.com>
2024-08-15 21:35:02 -0400

    assignment state update

--- src/app/teacher/view-assignments/page.tsx
+++ src/app/teacher/view-assignments/page.tsx
@@ -31,9 +31,12 @@ const ViewAssignments = () => {
 
   const [assignments, setAssignments] = useState<Assignment[]>([]);
   const [classes, setClasses] = useState<Class[]>([]);
-  const [selectedClass, setSelectedClass] = useState<string>("");
-  const [selectedDueDate, setSelectedDueDate] = useState<string>("");
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState<{ [key: string]: boolean }>(
+    {}
+  );
+  const [assignmentStates, setAssignmentStates] = useState<{
+    [key: string]: { selectedClass: string; selectedDueDate: string };
+  }>({});
 
   useEffect(() => {
     const fetchClasses = async () => {
@@ -47,7 +50,6 @@ const ViewAssignments = () => {
     const fetchAssignments = async () => {
       try {
         const res = await axios.get("/api/teacher/assignments/get-assignments");
-
         setAssignments(res.data.assignments);
         console.log("assignments are:", res.data.assignments);
       } catch (error) {
@@ -59,6 +61,8 @@ const ViewAssignments = () => {
   }, []);
 
   const handleAssign = async (assignmentId: string) => {
+    const { selectedClass, selectedDueDate } =
+      assignmentStates[assignmentId] || {};
     const assignmentToAssign = {
       assignmentId,
       classes: [{ classId: selectedClass, dueDate: selectedDueDate }],
@@ -79,9 +83,28 @@ const ViewAssignments = () => {
     }
   };
 
-  const handleSelectClass = (classId: string) => {
-    setSelectedClass(classId);
-    setDropdownOpen(false); // Close the dropdown after selection
+  const handleSelectClass = (assignmentId: string, classId: string) => {
+    setAssignmentStates((prevState) => ({
+      ...prevState,
+      [assignmentId]: {
+        ...prevState[assignmentId],
+        selectedClass: classId,
+      },
+    }));
+    setDropdownOpen((prevState) => ({
+      ...prevState,
+      [assignmentId]: false,
+    }));
+  };
+
+  const handleDueDateChange = (assignmentId: string, dueDate: string) => {
+    setAssignmentStates((prevState) => ({
+      ...prevState,
+      [assignmentId]: {
+        ...prevState[assignmentId],
+        selectedDueDate: dueDate,
+      },
+    }));
   };
 
   const isClassAssigned = (assignment: Assignment, classId: string) => {
@@ -96,64 +119,83 @@ const ViewAssignments = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
       {assignments &&
-        assignments.map((assignment) => (
-          <div
-            key={assignment._id}
-            className="bg-white p-4 rounded-md shadow-md"
-          >
-            <h2 className="text-lg font-semibold">{assignment.title}</h2>
-            <form>
-              <div className="mb-4 relative">
-                <label className="block text-gray-700">Select Class:</label>
-                <div
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none cursor-pointer"
-                  onClick={() => setDropdownOpen(!dropdownOpen)}
-                >
-                  {selectedClass
-                    ? classes.find((cls) => cls._id === selectedClass)?.name
-                    : "Select a class"}
-                  <span className="absolute top-2/4 mt-4 right-3 transform -translate-y-1/2">
-                    {dropdownOpen ? <FaChevronUp /> : <FaChevronDown />}
-                  </span>
+        assignments.map((assignment) => {
+          const { selectedClass = "", selectedDueDate = "" } =
+            assignmentStates[assignment._id] || {};
+          return (
+            <div
+              key={assignment._id}
+              className="bg-white p-4 rounded-md shadow-md"
+            >
+              <h2 className="text-lg font-semibold">{assignment.title}</h2>
+              <form>
+                <div className="mb-4 relative">
+                  <label className="block text-gray-700">Select Class:</label>
+                  <div
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none cursor-pointer"
+                    onClick={() =>
+                      setDropdownOpen((prevState) => ({
+                        ...prevState,
+                        [assignment._id]: !prevState[assignment._id],
+                      }))
+                    }
+                  >
+                    {selectedClass
+                      ? classes.find((cls) => cls._id === selectedClass)?.name
+                      : "Select a class"}
+                    <span className="absolute top-2/4 mt-4 right-3 transform -translate-y-1/2">
+                      {dropdownOpen[assignment._id] ? (
+                        <FaChevronUp />
+                      ) : (
+                        <FaChevronDown />
+                      )}
+                    </span>
+                  </div>
+                  {dropdownOpen[assignment._id] && (
+                    <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg mt-2 max-h-60 overflow-auto">
+                      {classes.map((cls) => (
+                        <li
+                          key={cls._id}
+                          onClick={() =>
+                            handleSelectClass(assignment._id, cls._id)
+                          }
+                          className={`px-4 py-2 cursor-pointer hover:bg-gray-100 flex justify-between ${
+                            selectedClass === cls._id ? "bg-gray-100" : ""
+                          }`}
+                        >
+                          {cls.name}
+                          {isClassAssigned(assignment, cls._id) && (
+                            <FaCheck className="text-green-500" />
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </div>
-                {dropdownOpen && (
-                  <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg mt-2 max-h-60 overflow-auto">
-                    {classes.map((cls) => (
-                      <li
-                        key={cls._id}
-                        onClick={() => handleSelectClass(cls._id)}
-                        className={`px-4 py-2 cursor-pointer hover:bg-gray-100 flex justify-between ${
-                          selectedClass === cls._id ? "bg-gray-100" : ""
-                        }`}
-                      >
-                        {cls.name}
-                        {isClassAssigned(assignment, cls._id) && (
-                          <FaCheck className="text-green-500" />
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700">Select Due Date:</label>
-                <input
-                  type="date"
-                  value={selectedDueDate}
-                  onChange={(e) => setSelectedDueDate(e.target.value)}
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-              <button
-                type="button"
-                onClick={() => handleAssign(assignment._id)}
-                className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
-              >
-                Assign to Class
-              </button>
-            </form>
-          </div>
-        ))}
+                <div className="mb-4">
+                  <label className="block text-gray-700">
+                    Select Due Date:
+                  </label>
+                  <input
+                    type="date"
+                    value={selectedDueDate}
+                    onChange={(e) =>
+                      handleDueDateChange(assignment._id, e.target.value)
+                    }
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handleAssign(assignment._id)}
+                  className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition"
+                >
+                  Assign to Class
+                </button>
+              </form>
+            </div>
+          );
+        })}
     </div>
   );
 };
